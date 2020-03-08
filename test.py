328aabe7716328f18ba062be378ce400a8388e38
lil_box <lil_box@lil-boxs-MacBook-Air.local>
2020-03-08 04:02:25 -0400

    adding main

--- test.py
+++ test.py
@@ -5,21 +5,29 @@ import os
 userhome = os.path.expanduser('~')
 csvFile = userhome + "/Projects/spark-test/test-csv/10681_2017_1876_MOESM1_ESM.csv"
 jsonFile = userhome + "/Projects/spark-test/test-json/10681_2017_1876_MOESM1_ESM.json"
-data =[]
-with open(csvFile) as f: 
-    reader = csv.DictReader(f)
-    for rows in reader:
-        # print(rows)
-        # for key in rows:
-        #     print(key)
-        j = {}
-        for key in rows:
-            j[key] = rows[key]
-        data.append(j)
-        
-print(json.dumps(data, indent=4))
 
-with open(jsonFile, 'w') as jsonFile:
+def conCSV2JSON(csvFile,jsonFile):
+    data =[]
+    with open(csvFile) as f: 
+        reader = csv.DictReader(f)
+        for rows in reader:
+            # print(rows)
+            # for key in rows:
+            #     print(key)
+            j = {}
+            for key in rows:
+                j[key] = rows[key]
+            data.append(j)
+            
+    print(json.dumps(data, indent=4))
 
-    jsonFile.write(json.dumps(data)) 
+    with open(jsonFile, 'w') as jsonFile:
+
+        jsonFile.write(json.dumps(data, indent=4))
+
+def main():
+    pass
+
+if "__name__" == "__main__":
+    main()
         
