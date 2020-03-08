0014fc0031761f143384a73853a5edd72873df59
lil_box <lil_box@lil-boxs-MacBook-Air.local>
2020-03-08 03:51:56 -0400

    creat json file

--- test.py
+++ test.py
@@ -4,14 +4,22 @@ import json
 import os
 userhome = os.path.expanduser('~')
 csvFile = userhome + "/Projects/spark-test/test-csv/10681_2017_1876_MOESM1_ESM.csv"
-
+jsonFile = userhome + "/Projects/spark-test/test-json/10681_2017_1876_MOESM1_ESM.json"
+data =[]
 with open(csvFile) as f: 
     reader = csv.DictReader(f)
     for rows in reader:
         # print(rows)
         # for key in rows:
         #     print(key)
+        j = {}
         for key in rows:
-            print(rows[key])
-        break
+            j[key] = rows[key]
+        data.append(j)
+        
+print(json.dumps(data, indent=4))
+
+with open(jsonFile, 'w') as jsonFile:
+
+    jsonFile.write(json.dumps(data)) 
         
