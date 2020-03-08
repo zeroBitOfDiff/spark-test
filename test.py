aaa5d2fe54eace0407ca9027d706dc1e171b9764
lil_box <lil_box@lil-boxs-MacBook-Air.local>
2020-03-08 06:04:42 -0400

    json file

--- test.py
+++ test.py
@@ -2,9 +2,7 @@
 import csv
 import json
 import os
-userhome = os.path.expanduser('~')
-csvFile = userhome + "/Projects/spark-test/test-csv/10681_2017_1876_MOESM1_ESM.csv"
-jsonFile = userhome + "/Projects/spark-test/test-json/10681_2017_1876_MOESM1_ESM.json"
+
 
 def conCSV2JSON(csvFile,jsonFile):
     data =[]
@@ -19,15 +17,40 @@ def conCSV2JSON(csvFile,jsonFile):
                 j[key] = rows[key]
             data.append(j)
             
-    print(json.dumps(data, indent=4))
+    #print(json.dumps(data, indent=4))
 
     with open(jsonFile, 'w') as jsonFile:
 
         jsonFile.write(json.dumps(data, indent=4))
 
-def main():
-    pass
+def breakUpJson(jsonFile,jsonFile2):
+    listOfJson = []
+    newListOfJson = []
+    with open(jsonFile) as jf:
+        listOfJson = json.loads(jf.read())
+        print(len(listOfJson))
+        for item in listOfJson:
+            newListOfJson.append(json.dumps(item) + '\n')
+    with open(jsonFile2, 'w') as jsonFile:
+        jsonFile.write(json.dumps(newListOfJson))
+# next line after each json file
 
-if "__name__" == "__main__":
+def viewJson(jsonFile):
+    with open(jsonFile) as j:
+        jf = json.loads(j.read())
+        print(json.dumps (jf, indent=4))
+    
+
+def main():
+    userhome = os.path.expanduser('~')
+    
+    j = userhome + "/Projects/spark-test/test-json/10681_2017_1876_MOESM1_ESM.json"
+    j2 = userhome + "/Projects/spark-test/test-json/t2.json"
+    print(j)
+    # breakUpJson(j,j2)
+    viewJson(j2)
+    
+
+if __name__ == '__main__':
     main()
         
